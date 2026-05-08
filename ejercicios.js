45a07e7dee318c43d4f720a79ca864660c15a86e
Gustavo <gustavo.soto.cardenas1994@gmail.com>
2026-05-08 11:24:31 -0400

    Primer commit

---
+++ ejercicios.js
@@ -0,0 +1,65 @@
+//Ejercicio 1
+let apellido = "Soto";
+console.log("\n-----------EJERCICIO 1");
+console.log(nombre,apellido);
+
+//Ejercicio 2
+let numero = 5;
+numero = (numero + 10) * 3
+console.log("\n-----------EJERCICIO 2");
+console.log(numero);
+
+
+//Ejercicio 3
+let comida = "cazuela"
+console.log("\n-----------EJERCICIO 3");
+console.log("Mi comida favorita es la " + comida)
+
+
+//Ejercicio 4
+let a = "10";
+let b = "3";
+console.log("\n-----------EJERCICIO 4");
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a % b)
+
+//Ejercicio 5
+let edad = 5;
+console.log("\n-----------EJERCICIO 5");
+if(edad > 10){
+    console.log("Es mayor a 10 años")
+} else {
+    console.log("No es mayor a 10 años")
+}
+
+//Ejercicio 6
+numero8 = 8
+console.log("es mayor a 5?", numero8 > 5);
+console.log("es menor a 10?", numero8 < 10);
+
+//Ejercicio 7
+let nota = 6;
+console.log("\n-----------EJERCICIO 7");
+console.log(nota == 7)
+
+let numero1 = 5;
+let numero2 = 10;
+
+//Ejercicio 8
+console.log("\n-----------EJERCICIO 8");
+console.log(numero1 == numero2)
+console.log(numero1 > numero2)
+console.log(numero1 < numero2)
+
+//Ejercisio 10
+console.log("\n-----------EJERCICIO 10");
+edad = 17;
+let tienePermiso = true;
+console.log( edad >= 18);
+
+//Ejercicio 11
+let contraseña = "1234";
+console.log("El usuario es",usuario,"y la contraseña es",contraseña);
+
